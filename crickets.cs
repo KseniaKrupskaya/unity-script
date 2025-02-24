1f3b9386015f221991f52accb40b5e822b2701b6
KseniaKrupskaya <slesarevakseni@gmail.com>
2025-02-24 13:02:48 +0300

    Create crickets.cs

---
+++ crickets.cs
@@ -0,0 +1,18 @@
+using System.Collections; //Импортируются стандартные библиотеки
+using System.Collections.Generic;
+using UnityEngine; //для юнити
+
+public class crickets : MonoBehaviour //Объявляем новый класс crickets - наследник MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("crickets"); //функция Debug.Log(), чтобы вывести строку crickets в консоль Unity
+    }
+}
